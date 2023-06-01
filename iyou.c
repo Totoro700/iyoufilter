234bf1ce8bea49c9aec2c1ca5703ae93439c43d2
Totoro700 <ianfrancisko@gmail.com>
2023-06-01 22:43:17 +0900

    c file

---
+++ iyou.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+
+void remove_spaces(char* s) {
+    char* d = s;
+    do {
+        while (*d == ' ') {
+            ++d;
+        }
+    } while (*s++ = *d++);
+}
+// i suck at c so prob doesn't work eeeeee
+const char* iyou(char[] msg) {
+	char[] m;
+	for(int i = 0; m[i]; i++){
+		m[i] = tolower(m[i]);
+	}
+	return remove_spaces(m)
+}
